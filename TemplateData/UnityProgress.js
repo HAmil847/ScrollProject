a0ddf0eb4e0ad11fa731025badd7444896d86158
HAmil847 <toimaxmermer12345@gmail.com>
2023-03-27 13:47:59 -0600

    js

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -1,12 +1,14 @@
 function UnityProgress(unityInstance, progress) {
   if (!unityInstance.Module)
     return;
-  if (!unityInstance.logo) {
+  if (unityInstance.logo) {
+    
     unityInstance.logo = document.createElement("div");
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {    
+  if (unityInstance.progress) {  
+    
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
     unityInstance.progress.empty = document.createElement("div");
@@ -17,8 +19,9 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.progress.appendChild(unityInstance.progress.full);
     unityInstance.container.appendChild(unityInstance.progress);
   }
-  unityInstance.progress.full.style.width = (100 * progress) + "%";
-  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
+  
+  //unityInstance.progress.full.style.width = (100 * progress) + "%";
+ // unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+  if (progress == -1)
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 }
